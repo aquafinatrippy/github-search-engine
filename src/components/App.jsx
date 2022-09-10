eee39a26d1ac383fd32bde592de479f8eeebdf11
unknown <taneldeviis@hotmail.com>
2022-09-10 23:52:57 +0300

    added routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,23 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
+import Home from "./Views/Home";
+import About from "./Views/About";
+import NotFound from "./Views/NotFound";
 
 const App = () => {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main className="container mx-auto px-3 pb-12">content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home></Home>} />
+            <Route path="/about" element={<About></About>} />
+            <Route path="/notfount" element={<NotFound></NotFound>} />
+            <Route path="/*" element={<NotFound></NotFound>} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
---
+++ src/components/Views/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="text-6xl">Welcome</div>
+    </div>
+  );
+};
+
+export default Home;
