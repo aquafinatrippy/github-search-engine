5ccf0a7f954655ce1df9b0c9f63334c166f22310
unknown <taneldeviis@hotmail.com>
2022-09-10 23:39:21 +0300

    Navbar comp start

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./layout/Navbar";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>content</main>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
